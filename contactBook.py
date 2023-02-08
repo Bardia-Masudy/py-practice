2589ace11a735d0473e5a53dd36557217bf46d88
Bardia Masudy <bmasudy74@gmail.com>
2023-02-07 19:00:43 -0800

    general additions, begun work on custom PDF parser

--- contactBook.py
+++ contactBook.py
@@ -2,27 +2,54 @@
 
 #with open('', 'w') as book
 
-class Contact(object):
-    def __init__(self) -> None:
-        response = []
+class Item(object):
+    def __init__(self, objType) -> None:
+        responses = []
+        match objType:
+            case 'edge':
+                questions = []
+            case 'hindrance':
+                questions = []
+            case 'power':
+                questions = []
+            case 'rule':
+                questions = []
+            case 'equipment':
+                questions = []
         # Prompts user for input upon creation.
-        for i in ['deal','profession','email address','phone number']: 
-            response.append(input(f"What is their {i}?:\n"))
-            print('')
-        self.notes, self.role, self.email, self.phone = response
+        for i in questions: 
+            responses.append(input(i))
+        self.info = responses
+        self.type = objType
+class InfoBook(object):
+    def __init__(self) -> None:
+        self.edges = []
+        self.hindrances = []
+        self.powers = []
+        self.rules = []
+        self.equipment = []
+    
+    def addItem(self, objType):
+        match objType:
+            case 'edge':
+                self.edges.append(Item(objType))
+            case 'hindrance':
+                self.hindrances.append(Item(objType))
+            case 'power':
+                self.powers.append(Item(objType))
+            case 'rule':
+                self.rules.append(Item(objType))
+            case 'equipment':
+                self.equipment.append(Item(objType))
+                
+book = InfoBook()
 
-contactBook = {}
+book.addItem('edge')
 
+print(book.edges[0])
 
-
-def addContact(name):
-    info = Contact()
-    contactBook.setdefault(name, info)
-
-def showBook():
-    for name, info in contactBook.values():
-        print(f'{name}:\n{info.notes}\n{info.role}\n{info.email} - {info.phone}')
-
-addContact('Jim')
-
-showBook()
+# Edge, Hindrance, Power, (Rules?), (Equipment?)
+# Edge: Name, Requirements, Rules Text, Origin Book + Page Number
+# Hindrance: Name, (Minor/Major), Rules Text, Origin Book + Page Number
+# Power: Name, Cost, Range, Duration, Rank, Rules Text, Modifiers, (Spell Lists?), Origin Book + Page Number
+# Item: Name, Rules Text (could be split into different items, weapon, armour, etc.), Origin Book + Page Number
